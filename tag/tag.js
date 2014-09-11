0e4ba765271cc9bb4d1f06873002954b9d8f13e7
Wray Bowling <wbowling@caktusgroup.com>
2014-09-11 19:48:48 -0400

    added faker to tag game. initial tag prototypes

---
+++ tag/tag.js
@@ -0,0 +1,30 @@
+/**
+* tag yer it
+*/
+
+function player(id,color){
+	this.id = id;
+	this.score = 0;
+}
+
+player.prototype
+
+
+////
+
+function turret(){
+
+}
+
+
+////
+
+
+function token(){
+
+}
+
+
+////
+////
+
