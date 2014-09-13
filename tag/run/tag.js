2a128e9baebdc2b51b9ded43e660769c778e968b
Wray Bowling <wbowling@caktusgroup.com>
2014-09-13 09:52:20 -0400

    just about everything works except scoring

--- tag/run/tag.js
+++ tag/run/tag.js
@@ -8,8 +8,7 @@ var players = [];
 var turrets = [];
 var bullets = [];
 var tokens = [];
-var i;
-var nextBullet = 0;
+var yerit = document.getElementById('yerit');
 
 canvasWidth = 1200;
 canvasHeight = 600;
@@ -32,8 +31,7 @@ function player(id){
 	this.element = document.getElementById('player_'+id);
 	this.radius = 30;
 	this.it = false;
-	this.x = undefined;
-	this.y = undefined;
+	this.score = 0;
 }
 
 player.prototype.hitTest = function(target){
@@ -44,6 +42,18 @@ player.prototype.advance = function(x,y){
 	this.x = x;
 	this.y = y;
 	this.element.setAttributeNS(null,'transform','translate('+x+','+y+')');
+	if(this.it){
+		yerit.setAttributeNS(null,'transform','translate('+x+','+y+')');
+		//console.log(circularHitTest(this,tokens[0]))
+		for(var t=0; t<tokens.length; t++){
+
+			if(circularHitTest(this,tokens[i])){
+				score += 100;
+				tokens[i].reset();
+			}
+		}
+	}
+
 };
 
 ////
@@ -53,6 +63,9 @@ function bullet(id){
 	this.velocity = 7;
 	this.element = document.getElementById('bullet_'+id);
 	this.reset();
+	this.owner = undefined; //player who shot this bullet
+	this.active = false;
+	this.radius = 10;
 }
 
 bullet.prototype.reset = function(){
@@ -74,6 +87,14 @@ bullet.prototype.advance = function(){
 			this.active = false;
 		}
 
+		for(var p=0; p<players.length; p++){
+			if(players[p].it === true && circularHitTest(this,players[p])){
+				players[p].it = false;
+				this.owner.it = true;
+				console.log(this.owner);
+			}
+		}
+
 		this.element.setAttributeNS(null,'transform','translate('+this.x+','+this.y+') rotate('+ this.angle/Math.PI/2*360 +')');
 	}
 };
@@ -87,26 +108,42 @@ bullet.prototype.hitTest = function(){
 function turret(id){
 	this.x = Math.random()*1024;
 	this.y = Math.random()*768;
+	this.radius = 100;
 	this.element = document.getElementById('turret_'+id);
 	return this;
+
 }
 
 turret.prototype.advance = function(){
 
 	for(var t=0; t<players.length; t++){
-		//not it players intersecting turret
+
+		//not-it players intersecting turret
 		if(players[t].it === false && circularHitTest(this,players[t])){
-			//var angle = (this.x - players[t].x) / (this.y - players[t].y);
-			bullets[nextBullet].x = this.x;
-			bullets[nextBullet].y = this.y;
-			//bullets[nextBullet].angle = angle;
-			bullets[nextBullet].active = true;
-			nextBullet = ++nextBullet % 10;
+			var bulletSearch = true;
+			var b=0;
+			var nextBullet = undefined;
+			while(bulletSearch === true && b < bullets.length){
+				if(bullets[b].active === false){
+					nextBullet = b;
+					bulletSearch = false;
+				}
+				b++;
+			}
+
+			if(nextBullet !== undefined){
+				var angle = Math.atan2((this.y - players[t].y),(this.x - players[t].x));
+				bullets[nextBullet].x = this.x;
+				bullets[nextBullet].y = this.y;
+				bullets[nextBullet].angle = angle;
+				bullets[nextBullet].active = true;
+				bullets[nextBullet].owner = players[t];
+			}
 		}
 	}
 	this.x += Math.random()*7 - 3.5;
 	this.y += Math.random()*7 - 3.5;
-	//this.radius += Math.random()*2 - 1;
+	this.radius += Math.random()*2 - 1;
 	//this.element.setAttributeNS(null,'r',this.radius);
 	this.element.setAttributeNS(null,'transform','translate('+this.x+','+this.y+')');
 	return this;
@@ -115,9 +152,11 @@ turret.prototype.advance = function(){
 ////
 
 function token(id){
-	this.x = Math.random()*1024;
-	this.y = Math.random()*768;
 	this.element = document.getElementById('token_'+id);
+	this.x = 0;
+	this.y = 0;
+	this.radius = 10;
+	this.reset();
 	return this;
 }
 
@@ -128,6 +167,11 @@ token.prototype.advance = function(){
 	return this;
 };
 
+token.prototype.reset = function(){
+	this.x = Math.random()*1024;
+	this.y = Math.random()*768;
+};
+
 ////
 ////
 
@@ -135,21 +179,24 @@ var redPlayer = new player('red');
 var greenPlayer = new player('green');
 var bluePlayer = new player('blue');
 players = [redPlayer,greenPlayer,bluePlayer];
+redPlayer.it = true;
 
-for(i=0; i<10; i++){
+for(var i=0; i<20; i++){
 	var new_bullet = new bullet(i);
 	bullets.push(new_bullet);
 }
 
-for(i=0; i<3; i++){
+for(var i=0; i<3; i++){
 	var new_turret = new turret(i);
 	turrets.push(new_turret);
 }
 
-for(i=0; i<10; i++){
-	new_token = new token(i);
+for(var i=0; i<10; i++){
+	var new_token = new token(i);
 	tokens.push(new_token);
 }
+
+console.log(tokens);
 
 //debugging faker subscription
 window.poll = function(data){
@@ -157,7 +204,6 @@ window.poll = function(data){
 	redPlayer.advance(data.red.x,data.red.y);
 	greenPlayer.advance(data.green.x,data.green.y);
 	bluePlayer.advance(data.blue.x,data.blue.y);
-
 };
 
 //live sensor subscription
@@ -170,17 +216,9 @@ var onSensorsMessage = onSensorsMessage || function(){console.error('please load
 onSensorsMessage(messageHandler, {});
 
 function tick(){
-	for(i=0; i<10; i++){
-		bullets[i].advance();
-	}
-
-	for(i=0; i<3; i++){
-		turrets[i].advance();
-	}
-
-	for(i=0; i<10; i++){
-		tokens[i].advance();
-	}
+	for(i=0; i<20; i++){ bullets[i].advance(); }
+	for(i=0; i<3; i++){ turrets[i].advance(); }
+	for(i=0; i<10; i++){ tokens[i].advance(); }
 }
 tick();
 
